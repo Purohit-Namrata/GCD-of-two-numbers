1c445e7ae54d4ece2c841e2ffdd82df0782f5dd0
Purohit-Namrata <namratapurohit78@gmail.com>
2024-08-21 15:37:28 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -5,12 +5,10 @@ def computeGCD(x,y):
 
 a=int(input("Enter number 1:"))
 b=int(input("Enter number 2:"))
-if(a<0):
-    abs(a)
-if(b<0):
-    abs(b)
-
-print("GCD is ",computeGCD(a,b))
+if(a<0 or b<0):
+    print("Enter positive number")
+else:
+    print("GCD is ",computeGCD(a,b))
 
 
 
