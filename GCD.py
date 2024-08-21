16f9a07a1f5198ea390742ba4388988306b1b2b5
Purohit-Namrata <namratapurohit78@gmail.com>
2024-08-21 15:29:45 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,12 +1,14 @@
 def computeGCD(x,y):
     while(y):
-        x,y=y,x%y
-        
+        x,y=y,x%y 
     return x
 
 a=int(input("Enter number 1:"))
 b=int(input("Enter number 2:"))
-
+if(a<0):
+    abs(a)
+if(b<0):
+    abs(b)
 
 print("GCD is ",computeGCD(a,b))
 
